2d7b082674b2cc35418e21354adade13103181d6
GGumGil <dongsilguy@gmail.com>
2022-12-15 14:43:16 +0900

    fix(): usecase 주석 추가

--- lib/domain/sample_usecase.dart
+++ lib/domain/sample_usecase.dart
@@ -2,4 +2,9 @@ class SampleUsecase {
   // 하드코딩된 결과를 반환하는 것이 독립적 presentation layer작업의 마지막.
   // Domain을 작업할 때는 이 곳에 Entity와 repository에 관련된 항목이 들어갈 것으로 예상.
   static String getDeviceName() => "이건 제 기계입니다.";
+
+  // static String getDeviceName() {
+  //   //실제 가져와야할 엔터티나 리포지터리의 메소드 호출이 들어와야겠죠.
+  //   //call, invoke
+  // }
 }
